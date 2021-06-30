28962550e6e58168736ff9ecfabf48fd12f82f0a
Omer Gulen <omergulen@jotform.com>
2021-06-30 23:04:10 +0300

    Update Google Analytics key

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-122322547-7`,
+        trackingId: `UA-122322547-8`,
       },
     },
     {
